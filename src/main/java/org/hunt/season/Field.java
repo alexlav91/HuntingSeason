bcfeefd0eae03668f1ec2ded5ba0545d1948df1e
Aleksandr Lopatin <Lav_91@bk.ru>
2022-05-31 16:07:05 +0400

    Hunter continue

--- src/main/java/org/hunt/season/Field.java
+++ src/main/java/org/hunt/season/Field.java
@@ -5,8 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Field {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Hunter hunter=new Hunter(context.getBean("goatBean", Animal.class));
+       // Hunter hunter=new Hunter(context.getBean("goatBean", Animal.class));
+        Hunter hunter = context.getBean("hunter",Hunter.class);
+
         hunter.killAnimal();
+        context.close();
 
 
     }
